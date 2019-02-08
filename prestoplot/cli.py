81e950a8dce6e766a1b9467719877bd379ac7790
David Eyk <david@eykd.net>
2019-02-08 06:48:36 -0800

    Add `--seed` option to CLI for pre-seeding oracles

--- prestoplot/cli.py
+++ prestoplot/cli.py
@@ -16,7 +16,8 @@ def main(debug):
 @click.option('--count', default=1, help="How many times to consult the oracle. Default 1.")
 @click.option('--markov-start', default='', help="Characters to start any Markov chains with, e.g. 'Ba'. Default ''.")
 @click.option('--markov-chainlen', default=2, help="Length of Markov chain links. Default 2.")
-def run(path, count, markov_start, markov_chainlen):
+@click.option('--seed', help="Pre-seed the random generator.")
+def run(path, count, markov_start, markov_chainlen, seed=None):
     '''Parse and consult a YAML generative grammar oracle file.
 
     The "oracle" consulted directly must include a `Begin:` stanza.
@@ -38,11 +39,14 @@ def run(path, count, markov_start, markov_chainlen):
     '''
     if markov_start and len(markov_start) < markov_chainlen:
         raise click.UsageError(f'--markov-start must be at least as long as --markov-chainlen, currently {markov_chainlen}.')
+    kwargs = dict(
+        start_markov=markov_start,
+        markov_chainlen=markov_chainlen,
+    )
+    if seed is not None:
+        kwargs['seed'] = seed
+
     for n in range(count):
-        print(story.render_story(
-            path,
-            start_markov=markov_start,
-            markov_chainlen=markov_chainlen,
-        ))
+        print(story.render_story(path, **kwargs))
         if n + 1 != count:
             print('\n---\n')
