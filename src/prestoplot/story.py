e5c5fc0e04d2ff1285b107d3c70a0ca90ef790d4
David Eyk <david@eykd.net>
2019-12-07 07:58:07 -0800

    Allow customization of start key when rendering story

--- src/prestoplot/story.py
+++ src/prestoplot/story.py
@@ -3,9 +3,9 @@ import logging
 from . import contexts, grammars, seeds
 
 
-def render_story(storage, name, **kwargs):
+def render_story(storage, name, start="Begin", **kwargs):
     logging.debug(f"Rendering {name} w/ {storage}")
     ctx = contexts.get_context(seeds.make_seed())
     ctx.update(kwargs)
     ctx = grammars.parse_grammar_file(storage, name, ctx)
-    return str(ctx["Begin"])
+    return str(ctx[start])
