f1ba749a534f7f238899263d8e14755ac40dfdad
David Eyk <david@eykd.net>
2022-05-08 22:18:41 -0700

    Fix versioneer

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
+import os
+import sys
+
 from setuptools import setup
 
-import versioneer
+sys.path.insert(0, os.path.dirname(__file__))
+
+import versioneer  # noqa: E402
 
 setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())
