6014e7a721cb4c5cfbd9a9b17230b23393b89dd6
David Eyk <david@eykd.net>
2025-08-10 15:45:18 -0700

    chore: run linters before tests

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 set -x
+
+# Automatically reformat code, but ignore breakpoint():
+exec uv run ruff check --fix --ignore T100
+exec uv run ruff format
+
 exec uv run python -m pytest \
     --failed-first \
     --exitfirst \
