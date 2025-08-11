9da2e180ab405c40f88e15ceecb642dbf98cf77b
David Eyk <david@eykd.net>
2025-08-10 17:16:50 -0700

    chore: tailor coverage skip

--- src/prestoplot/__init__.py
+++ src/prestoplot/__init__.py
@@ -4,7 +4,7 @@ try:
     from . import _version
 
     __version__ = _version.version
-except (ImportError, AttributeError):
+except (ImportError, AttributeError):  # pragma: no cover
     import importlib.metadata
 
     try:
